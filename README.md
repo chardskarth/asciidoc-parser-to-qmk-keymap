e9dbc0e97dedf691d3a305ccacb2aad3520effca
Richard Glen Domingo <chardskarth@gmail.com>
2019-02-18 22:31:55 +0800

    :construction: get blocks thats useful for parsing

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Let awesome asciidoc be single truth for qmk keymap source code
 
 ## TODO
-- [ ] index.js => load all mds, map and find markdown tables we want to parse
+- [x] index.js => load all mds, map and find markdown tables we want to parse
+- [ ] add useful mds in our list of docs to parse
+- [ ] create map of { keycode: string, description: string[] }
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { resolve } from "path";
 import Asciidoctor from "asciidoctor.js";
+import { flatten } from "lodash";
+
+const asciidoc = Asciidoctor();
 
 const QMK_DOCS_LOCATION = resolve(__dirname, "../qmk_firmware/docs/");
 const QMK_DOCS_FILES = [
@@ -8,7 +11,32 @@ const QMK_DOCS_FILES = [
   "feature_backlight"
 ].map(x => resolve(QMK_DOCS_LOCATION, `${x}.md`));
 
-const asciidoc = Asciidoctor();
+const asciiLoadAndDocumentMatches = fileLocation => {
+  const asciiDocument = asciidoc.loadFile(fileLocation);
 
-console.log(QMK_DOCS_FILES);
-console.log(asciidoc.loadFile(QMK_DOCS_FILES[0]));
+  const retVal = [];
+  const traverseBlock = obj => {
+    if (Array.isArray(obj)) {
+      obj.forEach(traverseBlock);
+    }
+    if ("blocks" in obj) {
+      traverseBlock(obj.blocks);
+    }
+    if ("lines" in obj) {
+      if (obj.lines.length) {
+        const linesLength = obj.lines.map(x => x.split("|").length);
+        const isMatch = linesLength.every(x => x === 5);
+        if (isMatch) {
+          console.log(`${obj.parent.converted_title} is a match`);
+          retVal.push(obj);
+        }
+      }
+    }
+  };
+
+  traverseBlock(asciiDocument.blocks);
+  return retVal;
+};
+
+const matchedTables = QMK_DOCS_FILES.map(asciiLoadAndDocumentMatches);
+console.log(flatten(matchedTables).map(x => x.parent.converted_title));
